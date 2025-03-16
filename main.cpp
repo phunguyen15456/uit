0c2df4ae8feacbf1314920b37393db146d036e26
phunguyen15456 <phunguyenbinh079@gmail.com>
2025-03-16 11:32:52 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,87 +1,28 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-struct ngaythangnam
-{
-    int ngay;
-    int thang;
-    int nam;
-};
-bool nhuan(ngaythangnam a)
-{
-    if (a.nam%400==0||(a.nam%4==0&&a.nam%100!=0)) return true;
-    return false;
+int binsearch(vector <int> a, int x){
+int l=0, r=a.size()-1, m;
+while (l<=r){
+    m=l+(r-l)/2;
+    if (a[m]==x) return m;
+    else if (a[m]>x) l=m+1;
+    else r=m-1;
 }
-void nhap(ngaythangnam &a)
-{
-    cout<<"NHAP NGAY: ";
-    cin >> a.ngay;
-    cout<<endl;
-    cout<<"NHAP THANG: ";
-    cin>>a.thang;
-    cout<< endl;
-    cout<<"NHAP NAM: ";
-    cin>>a.nam;
-}
-void xuat (ngaythangnam a)
-{
-    cout<<a.ngay<<"/"<<a.thang<<"/"<<a.nam;
-}
-int ngaycuathang(ngaythangnam a)
-{
-    int ngay;
-    switch(a.thang)
-    {
-        case 2:
-            if (nhuan(a)) ngay=29;
-            else ngay=28;
-            break;
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 8:
-        case 10:
-        case 12:
-            ngay=31;
-            break;
-        default:
-            ngay=30;
-    }
-    return ngay;
-}
-bool hople(ngaythangnam a)
-{
-    if (a.nam < 1582) return false;
-    if (a.thang < 1 || a.thang > 12) return false;
-    if (a.ngay < 1 || a.ngay > ngaycuathang(a)) return false;
-    return true;
-}
-int thucuangay(ngaythangnam a)
-{
-    int d = a.ngay;
-    int m = a.thang;
-    int y = a.nam;
-    if (m < 3)
-    {
-        m += 12;
-        y--;
-    }
-    int K=y%100;
-    int J=y/100;
-    int f=d+(13*(m+1))/5+K+(K/4)+(J/4)+(5*J);
-    return f % 7;
+return -1;
 }
 int main()
 {
-    ngaythangnam a;
-    nhap( a);
-    xuat(a);
-    cout<< " LA ";
-    int ngay=thucuangay( a);
-    if (ngay==1)
-    {
-        cout<<"CHU NHAT";
+    int n;
+vector <int> a;
+while (cin >> n) {
+        a.push_back(n);
     }
-    else cout << "THU "<<(ngay==0?7:ngay);
-    return 0;
+int i=-1;
+int idx=-1;
+while (idx==-1){
+    i--;
+    idx=binsearch(a,i);
+}
+cout << idx;
 }
